6be51a41cc2bc8e72cd6a1efd95397a13eb03e06
Vladislav <v.butirsky@gmail.com>
2023-02-24 22:11:06 +0700

    New format

--- src/svc/scheduler/tasks.py
+++ src/svc/scheduler/tasks.py
@@ -18,69 +18,77 @@ ALL_LINKS = []
 def parsing_part(self, try_num):
     try_num = int(try_num)
     try:
-            global OVERALL_RESULT
-            global ALL_LINKS
+        global OVERALL_RESULT
+        global ALL_LINKS
 
-            app_logger.info(f"Start part parsing number {try_num}")
+        length = len(ALL_LINKS)
 
-            file_name = "svc/handlers/geo_query.json"
-            file = Path(file_name)
+        app_logger.info(f"Start part parsing number {try_num}")
 
-            if file.exists():
+        file_name = "svc/handlers/geo_query.json"
+        file = Path(file_name)
 
-                with open(file_name, "r", encoding="utf-8") as json_file:
-                    data = json_file.read()
+        if file.exists():
 
-                if data:
-                    json_data = json.loads(data)
+            with open(file_name, "r", encoding="utf-8") as json_file:
+                data = json_file.read()
 
-                    if try_num == 1:
-                        if len(ALL_LINKS) > 1:
-                            parsed_data = parser.get_goods_data(ALL_LINKS[0])
-                            OVERALL_RESULT += parsed_data
-                            app_logger.info("Added 1st group")
-                        elif len(ALL_LINKS) == 1:
-                            parsed_data = parser.get_goods_data(ALL_LINKS[0])
-                            OVERALL_RESULT += parsed_data
-                            app_logger.info("Added 1st group and prepare file")
-                            fb_parser = TaskExecutor(json_data["Геопозиция"], json_data["Запрос"], json_data["chat_id"])
-                            fb_parser.storage = OVERALL_RESULT
-                            fb_parser.create_db_objects()
-                        else:
-                            app_logger.info("Nothing to parse")
-                    elif try_num == 2:
-                        if len(ALL_LINKS) > 2:
-                            parsed_data = parser.get_goods_data(ALL_LINKS[1])
-                            OVERALL_RESULT += parsed_data
-                            app_logger.info("Added 2nd group")
-                        elif len(ALL_LINKS) == 2:
-                            parsed_data = parser.get_goods_data(ALL_LINKS[1])
-                            OVERALL_RESULT += parsed_data
-                            app_logger.info("Added 2nd group and prepare file")
-                            fb_parser = TaskExecutor(json_data["Геопозиция"], json_data["Запрос"], json_data["chat_id"])
-                            fb_parser.storage = OVERALL_RESULT
-                            fb_parser.create_db_objects()
-                        else:
-                            app_logger.info("Nothing to parse")
-                    elif try_num == 3:
-                        if len(ALL_LINKS) == 3:
-                            parsed_data = parser.get_goods_data(ALL_LINKS[2])
-                            OVERALL_RESULT += parsed_data
-                            app_logger.info("Added 3d group and prepare file")
-                            fb_parser = TaskExecutor(json_data["Геопозиция"], json_data["Запрос"], json_data["chat_id"])
-                            fb_parser.storage = OVERALL_RESULT
-                            fb_parser.create_db_objects()
-                        else:
-                            app_logger.info("Nothing to parse")
-                    return "Done parsing"
+            if data:
+                json_data = json.loads(data)
 
-                else:
-                    app_logger.info("No query and geo data to start parsing, file is empty")
-                    return "No input data to parse, file is empty"
+                if try_num == 1:
+                    if length > 1:
+                        parsed_data = parser.get_goods_data(ALL_LINKS[0])
+                        OVERALL_RESULT += parsed_data
+                        app_logger.info("Added 1st group")
+                    elif length == 1:
+                        parsed_data = parser.get_goods_data(ALL_LINKS[0])
+                        OVERALL_RESULT += parsed_data
+                        app_logger.info("Added 1st group and prepare file")
+                        fb_parser = TaskExecutor(json_data["Геопозиция"], json_data["Запрос"], json_data["chat_id"])
+                        fb_parser.storage = OVERALL_RESULT
+                        fb_parser.create_db_objects()
+                        OVERALL_RESULT = []
+                        ALL_LINKS = []
+                    else:
+                        app_logger.info("Nothing to parse")
+                elif try_num == 2:
+                    if length > 2:
+                        parsed_data = parser.get_goods_data(ALL_LINKS[1])
+                        OVERALL_RESULT += parsed_data
+                        app_logger.info("Added 2nd group")
+                    elif length == 2:
+                        parsed_data = parser.get_goods_data(ALL_LINKS[1])
+                        OVERALL_RESULT += parsed_data
+                        app_logger.info("Added 2nd group and prepare file")
+                        fb_parser = TaskExecutor(json_data["Геопозиция"], json_data["Запрос"], json_data["chat_id"])
+                        fb_parser.storage = OVERALL_RESULT
+                        fb_parser.create_db_objects()
+                        OVERALL_RESULT = []
+                        ALL_LINKS = []
+                    else:
+                        app_logger.info("Nothing to parse")
+                elif try_num == 3:
+                    if length == 3:
+                        parsed_data = parser.get_goods_data(ALL_LINKS[2])
+                        OVERALL_RESULT += parsed_data
+                        app_logger.info("Added 3d group and prepare file")
+                        fb_parser = TaskExecutor(json_data["Геопозиция"], json_data["Запрос"], json_data["chat_id"])
+                        fb_parser.storage = OVERALL_RESULT
+                        fb_parser.create_db_objects()
+                        OVERALL_RESULT = []
+                        ALL_LINKS = []
+                    else:
+                        app_logger.info("Nothing to parse")
+                return "Done parsing"
 
             else:
-                app_logger.info("No query and geo data to start parsing, file does not exist")
-                return "No input data to parse, file does not exist"
+                app_logger.info("No query and geo data to start parsing, file is empty")
+                return "No input data to parse, file is empty"
+
+        else:
+            app_logger.info("No query and geo data to start parsing, file does not exist")
+            return "No input data to parse, file does not exist"
     except Exception as exc:
         print(exc)
         if self.request.retries >= self.max_retries:
